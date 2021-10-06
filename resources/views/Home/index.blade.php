72ba50921a1f3a9345d4139bd9a65e140afe9f18
GERALD ANTONIO RAMIREZ FONSECA <gerald.ramirezfonseca@ucr.ac.cr>
2021-10-06 12:46:54 -0600

    Se agrego el controlador Home y su vista index

---
+++ resources/views/Home/index.blade.php
@@ -0,0 +1,18 @@
+@extends('Layouts.menu')
+
+@section('title', "Naviera PeP")
+
+@section('styles')
+    <link href="{{ URL:: asset('css/home_index.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="css/home_slider.css">
+@endsection
+
+@section('scripts')
+    
+@endsection
+
+@section('body')
+
+<x-home_slider></x-home_slider>
+
+@endsection
